5e8c85a4c32d73cb3949e2a3333cbd53cd1ac253
Farman71 <farmanhussain71@gmail.com>
2025-01-23 17:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let status=document.getElementById("status");
+let button=document.getElementById("enterBtn");
+function metaVerse() {
+	status.innerHTML="Entered Metaverse";
+	status.style.fontSize="40px";
+	status.style.fontWeight="bolder";
+	button.style.fontSize="20px";
+}
